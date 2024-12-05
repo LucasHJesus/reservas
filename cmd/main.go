e0b24e1d808a00a5db73bd85d9aa47b289af439c
LucasHJesus <lukaslima641@gmail.com>
2024-12-05 12:22:36 -0300

    conexão com banco

--- cmd/main.go
+++ cmd/main.go
@@ -4,9 +4,20 @@ import (
 	"log"
 
 	"github.com/LucasHJesus/reservas/cmd/api"
+	"github.com/LucasHJesus/reservas/db"
+	"github.com/go-sql-driver/mysql"
 )
 
 func main() {
+	db, err := db.NewSQLStorage(mysql.Config{
+		User:                 "rooot",
+		Passwd:               "asd",
+		Addr:                 "127.0.1:3300",
+		DBName:               "reservas",
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
 	server := api.NewAPIServer(":8080", nil)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
